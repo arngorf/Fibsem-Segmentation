164f8312a422daf041baeef919933951b19ede6d
Hans <arngorf@gmail.com>
2018-03-13 20:05:57 +0100

    multiple session training test

--- src/main.py
+++ src/main.py
@@ -150,8 +150,8 @@ def single_train():
     model_class = model_manager.get_model(model_name)
     input_shape = model_class.input_shape
 
-    #model_class.summary()
-    #exit()
+    model_class.summary()
+    exit()
 
     model_class.set_session('default')
 
@@ -173,8 +173,66 @@ def single_train():
                 avg_grad_stop=True,
                 )
 
+train_n_time(n):
+    #dataset_path = '../data/lausanne'
+    dataset_path = '/scratch/xkv467/lausanne'
+    results_path = '../results'
+    batch_size = 32
+    img_class_map = [[0, 3, 4, 5, 6, 7, 8], [1,2]]
+    num_classes = len(img_class_map)
+    norm_params = (142.1053396892233, 30.96410819657719)
+
+    model_manager = ModelsManager(results_path)
+
+    conv_dropout_p = 0.5
+    dense_dropout_p = 0.5
+
+    session_names = ['run_' + str(i+1) for i in range(n)]
+
+    train_params = conv_2_layer.make_model(num_classes,
+                                           conv_dropout_p=conv_dropout_p,
+                                           dense_dropout_p=dense_dropout_p,
+                                           norm_params=norm_params,
+                                           )
+
+    model, model_name, input_shape = train_params
+
+    model_manager.new_model(model,
+                            model_name,
+                            input_shape,
+                            num_classes,
+                            lr = 0.001,
+                            )
+
+    model_class = model_manager.get_model(model_name)
+    input_shape = model_class.input_shape
+
+    for session_name in session_names:
+
+        model_class.set_session(session_name)
+
+        dataset = PatchDataset(dataset_path,
+                               batch_size,
+                               input_shape,
+                               img_class_map,
+                               norm_params=norm_params,
+                               )
+
+        iterations_per_epoch=524288
+        max_epochs=32
+
+        train_model(dataset,
+                    model_class,
+                    batch_size,
+                    iterations_per_epoch,
+                    max_epochs,
+                    avg_grad_stop=True,
+                    avg_grad_n=16,
+                    )
+
 if __name__ == '__main__':
 
     #mini_test()
     #dropbox_effect()
-    single_train()
+    #single_train()
+    train_n_time(3)
