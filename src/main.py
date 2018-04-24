63279d982b4d1a754766c1d144efa6fef372433c
Hans <arngorf@gmail.com>
2018-04-24 20:54:48 +0200

    ...

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ import numpy as np
 #import matplotlib.pyplot as plt
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] ="2"
+os.environ["CUDA_VISIBLE_DEVICES"] ="0"
 
 from ModelsManager import ModelsManager
 from PatchDataset import PatchDataset
